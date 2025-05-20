ed44e70302af0a77ecd3bfc0285e317be726c9bd
sa-abdullah <salaudeenabdu@gmail.com>
2025-05-20 09:33:16 +0100

    built a bot that can check Ethereum transactions directly on social media Dm.

---
+++ telegram.js
@@ -0,0 +1,49 @@
+import { Telegraf } from 'telegraf'
+import { InfuraProvider } from 'ethers'
+import 'dotenv/config'
+import { queryGroq, createMCPContext } from './handlePrompt.js'
+
+
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+const BOT_USERNAME = '@LedgerLook'
+const provider = new InfuraProvider('sepolia', process.env.INFURA_PROJECT_ID)
+
+export const runTelegramBot = () => {
+    bot.start((ctx) => {
+        ctx.reply(`👋Hey! I'm ${BOT_USERNAME}. Send me a tx hash to check transactions. Or ask me something`)
+    })
+
+    bot.on('text', async (ctx) => {
+        const text = ctx.message.text.trim();
+        const txHashMatch = text.match(/0x[a-fA-F0-9]{64}/g)
+
+        if (!txHashMatch) {
+            await ctx.reply(await queryGroq(text))
+            return 
+        }
+
+        const txHash = txHashMatch[0]
+        await ctx.reply('Fetching transaction details for:\n${txHash}')
+
+        try {
+            const transaction = await provider.getTransaction(txHash)
+            if (!transaction) {
+                await ctx.reply('Transaction not found or still pending. Check your Ethereum transaction Hash')
+                return
+            }
+
+            const mcpContext = createMCPContext(transaction)
+            await ctx.reply(await queryGroq(mcpContext))
+
+        } catch (error) {
+            console.error('Error fetching transaction: ', error);
+            await ctx.reply('Error fetching transaction. Please try again later')
+        }
+    })
+
+    bot.launch();
+    console.log('Telegram Ethereum transaction bot is running...');
+}
+
+
